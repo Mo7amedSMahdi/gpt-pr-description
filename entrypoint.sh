585f92d5ac5ba1ba1616ce61bfdd73dc7fcb0b7a
Mo7amedSMahdi <mo7ammed.salah@outlook.com>
2023-07-09 09:56:44 +0300

    🔀 Update action.yml 🎨 Refactor: Rename action name and description
    The commit updates the action.yml file, renaming the action name and description to better reflect its purpose. The new name is "Autofill PR Description with OpenAI" and the new description highlights the functionality of autofilling pull request descriptions using OpenAI and ChatGPT.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 set -eu
 
 if [ -z "$INPUT_PULL_REQUEST_ID" ]; then
-    pull_request_id="$(jq "if (.issue.number != null) then .issue.number else .number end" < "$GITHUB_EVENT_PATH")"
+  pull_request_id="$(jq "if (.issue.number != null) then .issue.number else .number end" < "$GITHUB_EVENT_PATH")"
 
-    if [ "$pull_request_id" = "null" ]; then
-        echo "Failed to get pull request number from context, exiting..."
-        exit 0
-    fi
+  if [ "$pull_request_id" == "null" ]; then
+    echo "Could not find the pull request ID. Is this a pull request?"
+    exit 0
+  fi
 else
-    pull_request_id="$INPUT_PULL_REQUEST_ID"
+  pull_request_id="$INPUT_PULL_REQUEST_ID"
 fi
 
-/action/generate_pr.py \
-    --github-api-url "$GITHUB_API_URL" \
-    --github-token "$INPUT_GITHUB_TOKEN" \
-    --pull-request-id "$pull_request_id" \
-    --github-repository "$GITHUB_REPOSITORY" \
-    --openai-api-key "$INPUT_OPENAI_API_KEY" \
+/action/autofill_description.py \
+  --github-api-url "$GITHUB_API_URL" \
+  --github-repository "$GITHUB_REPOSITORY" \
+  --pull-request-id "$pull_request_id" \
+  --github-token "$INPUT_GITHUB_TOKEN" \
+  --openai-api-key "$INPUT_OPENAI_API_KEY" \
--- generate_pr.py
+++ generate_pr.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
-
 import sys
 import requests
-import json
-import os
 import argparse
+import json
 import openai
-
+import os
 
 SAMPLE_PROMPT = """
 Write a pull request description focusing on the motivation behind the change and why it improves the project.
@@ -41,123 +39,185 @@ Changes in file test/CommandParserTest.cpp: @@ -566,7 +566,7 @@ TEST(CommandPars
                         .withArgs<int, long, unsigned long, long long, unsigned long long, float, double, long double>();
 """
 
-RESPONSE_SAMPLE = """
+GOOD_SAMPLE_RESPONSE = """
 Currently, our CI build does not include Valgrind as part of the build and test process. Valgrind is a powerful tool for detecting memory errors, and its use is essential for maintaining the integrity of our project.
 This pull request adds Valgrind to the CI build, so that any memory errors will be detected and reported immediately. This will help to prevent undetected memory errors from making it into the production build.
 
 Overall, this change will improve the quality of the project by helping us detect and prevent memory errors.
 """
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Generate a pull request description using GPT-3')
-    parser.add_argument("--github-api-url",type=str,help="Github API URL",default="https://api.github.com",required=True)
-    parser.add_argument("--github-token",type=str,help="Github token",required=True)
-    parser.add_argument("--github-repo",type=str,help="Github repo",required=True)
-    parser.add_argument("--pull-request-id",type=int,help="Github PR id",required=True)
-    parser.add_argument("--openai-api-key",type=str,help="OpenAI API key",required=True)
-    parser.add_argument("--allowed-users",type=str,help="A comma-separated list of GitHub usernames that are allowed to trigger the action, empty or missing means all users are allowed",required=False)
-    
+    parser = argparse.ArgumentParser(
+        description="Use ChatGPT to generate a description for a pull request."
+    )
+    parser.add_argument(
+        "--github-api-url", type=str, required=True, help="The GitHub API URL"
+    )
+    parser.add_argument(
+        "--github-repository", type=str, required=True, help="The GitHub repository"
+    )
+    parser.add_argument(
+        "--pull-request-id",
+        type=int,
+        required=True,
+        help="The pull request ID",
+    )
+    parser.add_argument(
+        "--github-token",
+        type=str,
+        required=True,
+        help="The GitHub token",
+    )
+    parser.add_argument(
+        "--openai-api-key",
+        type=str,
+        required=True,
+        help="The OpenAI API key",
+    )
+    parser.add_argument(
+        "--allowed-users",
+        type=str,
+        required=False,
+        help="A comma-separated list of GitHub usernames that are allowed to trigger the action, empty or missing means all users are allowed",
+    )
     args = parser.parse_args()
 
     github_api_url = args.github_api_url
+    repo = args.github_repository
     github_token = args.github_token
-    github_repo = args.github_repo
     pull_request_id = args.pull_request_id
     openai_api_key = args.openai_api_key
     allowed_users = os.environ.get("INPUT_ALLOWED_USERS", "")
     if allowed_users:
         allowed_users = allowed_users.split(",")
-    openai_model = os.environ.get("INPUT_OPENAI_MODEL", "gpt-3.5-turbo")
-    max_prompt_token = int(os.environ.get("INPUT_MAX_PROMPT_TOKEN", "1048"))
-    openai_temperature = float(os.environ.get("INPUT_OPENAI_TEMPERATURE", "0.7"))
+    open_ai_model = os.environ.get("INPUT_OPENAI_MODEL", "gpt-3.5-turbo")
+    max_prompt_tokens = int(os.environ.get("INPUT_MAX_TOKENS", "1000"))
+    model_temperature = float(os.environ.get("INPUT_TEMPERATURE", "0.6"))
     model_sample_prompt = os.environ.get("INPUT_MODEL_SAMPLE_PROMPT", SAMPLE_PROMPT)
     model_sample_response = os.environ.get(
-        "INPUT_MODEL_SAMPLE_RESPONSE", RESPONSE_SAMPLE
+        "INPUT_MODEL_SAMPLE_RESPONSE", GOOD_SAMPLE_RESPONSE
     )
-    auth_header = {"Accept":"application/vnd.github.v3+json","Authorization":f"token %s" % github_token}
+    authorization_header = {
+        "Accept": "application/vnd.github.v3+json",
+        "Authorization": "token %s" % github_token,
+    }
 
-    pull_request_url = f"{github_api_url}/repos/{github_repo}/pulls/{pull_request_id}"
-    pull_request_result = requests.get(pull_request_url,headers=auth_header)
-
+    pull_request_url = f"{github_api_url}/repos/{repo}/pulls/{pull_request_id}"
+    pull_request_result = requests.get(
+        pull_request_url,
+        headers=authorization_header,
+    )
     if pull_request_result.status_code != requests.codes.ok:
-        print(f"Failed to get pull request {pull_request_id} from {github_repo}" + str(pull_request_result.status_code))
-        return 0
+        print(
+            "Request to get pull request data failed: "
+            + str(pull_request_result.status_code)
+        )
+        return 1
     pull_request_data = json.loads(pull_request_result.text)
 
-    if pull_request_data['body']:
-        print("Pull request already has a description")
+    if pull_request_data["body"]:
+        print("Pull request already has a description, skipping")
         return 0
-    
+
     if allowed_users:
-        pull_request_author = pull_request_data['user']['login']
-        if pull_request_author not in allowed_users:
-            print(f"Pull request author {pull_request_author} is not allowed to trigger the action")
+        pr_author = pull_request_data["user"]["login"]
+        if pr_author not in allowed_users:
+            print(
+                f"Pull request author {pr_author} is not allowed to trigger this action"
+            )
             return 0
-        
-    pull_request_title = pull_request_data['title']
+
+    pull_request_title = pull_request_data["title"]
+
+    pull_request_files = []
     # Request a maximum of 10 pages (300 files)
-    for page_num in range (1,11):
-        pull_request_files_url = f"{github_api_url}/files?page={page_num}&per_page=30"
-        pull_request_files_result = requests.get(pull_request_files_url,headers=auth_header)
+    for page_num in range(1, 11):
+        pull_files_url = f"{pull_request_url}/files?page={page_num}&per_page=30"
+        pull_files_result = requests.get(
+            pull_files_url,
+            headers=authorization_header,
+        )
 
-        if pull_request_files_result.status_code != requests.codes.ok:
-            print(f"Failed to get pull request {pull_request_id} files from {github_repo}" + str(pull_request_files_result.status_code))
-            return 0
-        
-        pull_request_files_data = json.loads(pull_request_files_result.text)
+        if pull_files_result.status_code != requests.codes.ok:
+            print(
+                "Request to get list of files failed with error code: "
+                + str(pull_files_result.status_code)
+            )
+            return 1
 
-        if len(pull_request_files_data) == 0:
+        pull_files_chunk = json.loads(pull_files_result.text)
+
+        if len(pull_files_chunk) == 0:
             break
-        ai_prompt = f"""
+
+        pull_request_files.extend(pull_files_chunk)
+
+        completion_prompt = f"""
 Write a pull request description focusing on the motivation behind the change and why it improves the project.
 Go straight to the point.
 
 The title of the pull request is "{pull_request_title}" and the following changes took place: \n
 """
-        for file in pull_request_files_data:
-            # Not all PR file metadata entries may contain a patch section
-            # For example, entries related to removed binary files may not contain it
-            if 'patch' not in file:
-                continue
-            file_name = file['filename']
-            file_patch = file['patch']
-            ai_prompt += f"Changes in file {file_name}: {file_patch}\n"
+    for pull_request_file in pull_request_files:
+        # Not all PR file metadata entries may contain a patch section
+        # For example, entries related to removed binary files may not contain it
+        if "patch" not in pull_request_file:
+            continue
 
-        
-        max_allowed_tokens = 2048
-        chars_per_token = 4
-        max_allowed_chars = max_prompt_token * chars_per_token
-        if len(ai_prompt) > max_allowed_chars:
-            ai_prompt = ai_prompt[:max_allowed_chars]
+        filename = pull_request_file["filename"]
+        patch = pull_request_file["patch"]
+        completion_prompt += f"Changes in file {filename}: {patch}\n"
 
-        openai.api_key = openai_api_key
-        openai_response = openai.Completion.create(
-            model=openai_model,
-            messages=[{
-                "role":"system",
-                "content":"You are a helpful assistant who writes pull request descriptions",
+    max_allowed_tokens = 2048  # 4096 is the maximum allowed by OpenAI for GPT-3.5
+    characters_per_token = 4  # The average number of characters per token
+    max_allowed_characters = max_allowed_tokens * characters_per_token
+    if len(completion_prompt) > max_allowed_characters:
+        completion_prompt = completion_prompt[:max_allowed_characters]
+
+    openai.api_key = openai_api_key
+    openai_response = openai.ChatCompletion.create(
+        model=open_ai_model,
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant who writes pull request descriptions",
             },
             {"role": "user", "content": model_sample_prompt},
             {"role": "assistant", "content": model_sample_response},
-            {"role": "user", "content": ai_prompt},
-            ],
-            temperature=openai_temperature,
-            max_tokens=max_prompt_token,
-        )
-        generated_pr_description = openai_response.choices[0].message.content
-        redundant_prefix = "This pull request description was generated by an AI assistant."
-        if generated_pr_description.startswith(redundant_prefix):
-            generated_pr_description = generated_pr_description[len(redundant_prefix):]
-        generated_pr_description = generated_pr_description[0].upper() + generated_pr_description[1:]
-        print(f"Generated PR description:\n{generated_pr_description}")
-        issues_url = "%s/repos/%s/issues/%s" % (github_api_url,github_repo,pull_request_id)
-        update_pr_description_result = requests.patch(issues_url,headers=auth_header,json={"body":generated_pr_description})
+            {"role": "user", "content": completion_prompt},
+        ],
+        temperature=model_temperature,
+        max_tokens=max_prompt_tokens,
+    )
 
-        if update_pr_description_result.status_code != requests.codes.ok:
-            print(f"Failed to update pull request {pull_request_id} description from {github_repo}" + str(update_pr_description_result.status_code))
-            print("Response: " + update_pr_description_result.text)
-            return 1
-        
-    if __name__ == "__main__":
-        sys.exit(main())
-        
+    generated_pr_description = openai_response.choices[0].message.content
+    redundant_prefix = "This pull request "
+    if generated_pr_description.startswith(redundant_prefix):
+        generated_pr_description = generated_pr_description[len(redundant_prefix) :]
+        generated_pr_description = (
+            generated_pr_description[0].upper() + generated_pr_description[1:]
+        )
+    print(f"Generated pull request description: '{generated_pr_description}'")
+    issues_url = "%s/repos/%s/issues/%s" % (
+        github_api_url,
+        repo,
+        pull_request_id,
+    )
+    update_pr_description_result = requests.patch(
+        issues_url,
+        headers=authorization_header,
+        json={"body": generated_pr_description},
+    )
+
+    if update_pr_description_result.status_code != requests.codes.ok:
+        print(
+            "Request to update pull request description failed: "
+            + str(update_pr_description_result.status_code)
+        )
+        print("Response: " + update_pr_description_result.text)
+        return 1
+
+
+if __name__ == "__main__":
+    sys.exit(main())
